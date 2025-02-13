645ccaa8bc8a617b56d1df525a2e711a20e4b88e
TerryR23 <165734851+TerryR23@users.noreply.github.com>
2025-02-13 08:14:12 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,117 @@
+//
+//  main.c
+//  Simple-Key-Management
+//
+//  Created by Randy Terry on 10/8/24.
+//
+
+#include <stdio.h>
+#include <semaphore.h>
+#include <string.h>
+#include <strhash.h>
+#include <unistd.h>
+#include <sys/random.h>
+#include <stdlib.h>
+
+
+
+
+#define HASH_Algo "SHA256"
+
+struct Certificate {
+    char Device_Num;
+    unsigned char ID_Num;
+    char Device_Name[30];
+    char CA_Name[30];
+    char *Key_Derivation_Function;
+    
+};
+struct Device {
+    int Device_Num;
+    unsigned char ID_num;
+    const char KDF;
+    unsigned char Key[32];
+    char Device_Name[30];
+    char * Connections[];
+};
+
+struct Cert_Auth{
+    int Device_Num;
+    unsigned char ID_Num;
+    int port_Num;
+    char Device_Name[30];
+    int Num_of_Devices;
+    struct Device * Devices[5];
+
+};
+
+
+int main( int argc, const char * argv[]);
+int Create_Device(struct Cert_Auth * CA, char NAME[27]);
+int Get_Bytes(unsigned char *dest, int NUMBYTES);
+
+int block_Cipher( int messagelen);
+
+int AES_CBC(unsigned char *)
+
+
+
+int main(int argc, const char * argv[]) {
+    int set_mode = 0;
+        if (argc <2){
+            puts("Error, missing possitional arguement. Please try again");
+            return -1;
+        }
+       /* Reads command line to see if it's the device or CA (will be removed when different devices used) */
+    
+    for (int i =0; i< argc; i++){
+        char flag = *argv[i];
+        printf("%c",flag);
+        switch (flag){
+            case 'C':
+                puts("Running as a Certificate Authority");
+                set_mode = 1;
+                
+            case 'D':
+                puts("Running in Device mode");
+                set_mode = 2;
+        }
+        
+    }
+    if (set_mode == 0){
+        puts("Mode not identified, please try again");
+        return -1;
+    }
+    
+    if (set_mode == 1){
+        struct Cert_Auth Host;
+        Get_Bytes((unsigned char *) Host.Device_Name, 30);
+        Get_Bytes(&Host.ID_Num, 1);
+        Host.port_Num = 65000;
+        Host.Device_Num = 1;
+        Host.Num_of_Devices = 0;
+        
+    }
+    
+    
+
+    printf("Hello, World!\n");
+    return 0;
+}
+
+/* This function is called when a device reaches out. Used by the CA, this creates and fills a Certificate and returns it to the device*/
+int Create_Device(struct Cert_Auth * CA, char NAME[]){
+    struct Certificate new_cert;
+    new_cert.Device_Num = CA->Num_of_Devices + 2;
+    if ((strcpy(new_cert.CA_Name, CA->Device_Name)) != 0){ puts("Error Copying Certificate Authority Name \n"); return -1;};
+    if ((strcpy(new_cert.Device_Name, NAME)) != 0){puts("Error Copying Device Name \n"); return -1;};
+    if ((Get_Bytes(&new_cert.ID_Num, 1)) != 0){puts("Error Retriving Bytes\n"); return -1;};
+    new_cert.Key_Derivation_Function = HASH_Algo;
+    
+    return 0;
+};
+
+int Get_Bytes(unsigned char *dest, int NUMBYTES){
+    arc4random_buf(dest, NUMBYTES);
+    return 0;
+};
